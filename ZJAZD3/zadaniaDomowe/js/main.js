754d09fe74541aefe84624b79224326c20448144
ASlawinska <slawinska.adrianna@gmail.com>
2021-01-01 19:35:31 +0100

    Edition code in 3 exrcises - more universal solve.

--- ZJAZD3/zadaniaDomowe/js/main.js
+++ ZJAZD3/zadaniaDomowe/js/main.js
@@ -30,20 +30,55 @@
 // console.log(filterDigitLength([2, 7, 8, 9, 1012], 1));
 // console.log(filterDigitLength([32, 88, 74, 91, 300, 4050], 1));
 // console.log(filterDigitLength([5, 6, 8, 9], 1));
+// function filterDigitLength(arr, n) {
+//     let countArr = arr.map((el,i) => {
+//     if (i=0, el>=1, i++) {
+//         el/=10
+//         return i
+//         console.log(counrArr);
+//     } else {
+//         return ['']
+//     }
+// })}
 
-//zad 3
-//
-function getExtension(arr) {
-    let typeFile = arr.map(el => el.split('.'));
-    let newArr = []
-    for (let i = 0; i < typeFile.length; i++) {
-        newArr.push(typeFile[i][1]);
-        // console.log(newArr);
-
-    }
-return console.log(newArr);
+function filterDigitLength(arr, n) {
+    let countArr = arr.filter(function(x) {
+        if(((x / Math.pow(10, n-1)) >= 1) && (x / Math.pow(10, n-1)) < 10)
+        {
+            return x;
+        }
+    })
+    return countArr
 }
 
-getExtension(["code.html", "code.css"])
-getExtension(["project1.jpg", "project1.pdf", "project1.mp3"])
 
+console.log(filterDigitLength([88, 232, 4, 9721, 100, 999, 555], 3));
+console.log(filterDigitLength([88, 232, 4, 9721, 555], 3));
+console.log(filterDigitLength([2, 7, 8, 9, 1220], 1));
+console.log(filterDigitLength([32, 88, 74, 91, 300, 4050], 1));
+console.log(filterDigitLength([5, 6, 8, 9], 1));
+//zad 2 inny sposób 
+// filterDigitLength = (arr, x) => {
+//     return arr.filter(item => item.toString().length === x);
+// }
+
+//zad 3
+// //
+// function getExtension(arr) {
+//     let typeFile = arr.map(el => el.split('.'));
+//     let newArr = []
+//     for (let i = 0; i < typeFile.length; i++) {
+//         newArr.push(typeFile[i][1]);
+//         // console.log(newArr);
+
+//     }
+// return newArr;
+// }
+//inny sposób 
+//getExtension = (arr) => {
+//     return arr.map(item => item.slice(((item.indexOf('.')+1))))
+
+// }
+
+// getExtension(["code.html", "code.css"])
+// getExtension(["project1.jpg", "project1.pdf", "project1.mp3"])
