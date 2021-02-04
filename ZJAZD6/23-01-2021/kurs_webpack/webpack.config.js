1944f37e6a7fa771d4a3b232a921c94dad5c0d6b
ASlawinska <slawinska.adrianna@gmail.com>
2021-02-04 22:54:27 +0100

    Untracked node modules

--- ZJAZD6/23-01-2021/kurs_webpack/webpack.config.js
+++ ZJAZD6/23-01-2021/kurs_webpack/webpack.config.js
@@ -3,7 +3,32 @@ const path = require("path");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.resolve(_dirname, "dist"),
+        path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
-    }
+    },
+    devServer:{
+        contentBase: path.join(__dirname,"dist"),
+        port: 9000,
+        watchContentBase: true
+        },
+    module: {
+        rules: [{
+        test: /\.css$/,
+        use: [
+        {loader: "style-loader/url"},
+        {loader: "file-loader",
+        options: {name: "[name].[ext]"}}
+        ]
+        }]
+        },
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    persets: ['@babel/preset-env']
+                }
+            }
+        }
 }
